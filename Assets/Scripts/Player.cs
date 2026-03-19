0aa1a1a631a86887198261bf2b455ffdf8d3f8f0
Justin <justinmnmj@gmail.com>
2026-03-18 20:28:00 -0700

    animations added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     
     [SerializeField] private float moveSpeed = 7f;
+    
+    public bool IsWalking { get; private set; }
 
     private void Update() {
         var inputVector = new Vector2(0,0);
@@ -26,6 +28,8 @@ public class Player : MonoBehaviour {
         
         var tr = transform;
         tr.position += moveDir * (moveSpeed * Time.deltaTime);
+        
+        IsWalking = moveDir != Vector3.zero;
         
         const float rotateSpeed = 10f;
         tr.forward = Vector3.Slerp(tr.forward, moveDir, rotateSpeed * Time.deltaTime);
---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private static readonly int IsWalking = Animator.StringToHash("IsWalking");
+    private Animator animator;
+    [SerializeField] private Player player;
+    
+    private void Awake() {
+        animator = GetComponent<Animator>();
+        animator.SetBool(IsWalking, player.IsWalking);
+    }
+
+    private void Update() {
+        animator.SetBool(IsWalking, player.IsWalking);
+    }
+}
