32e830d3caad76b28d38a278347e84ab6c85dc66
Justin <justinmnmj@gmail.com>
2026-03-18 20:33:15 -0700

    added cinemachine

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,38 @@
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    
     [SerializeField] private float moveSpeed = 7f;
-    
+
     public bool IsWalking { get; private set; }
 
     private void Update() {
-        var inputVector = new Vector2(0,0);
+        var inputVector = new Vector2(0, 0);
 
         if(Input.GetKey(KeyCode.W)) {
             inputVector.y += 1;
         }
+
         if(Input.GetKey(KeyCode.S)) {
             inputVector.y -= 1;
         }
+
         if(Input.GetKey(KeyCode.A)) {
             inputVector.x -= 1;
         }
+
         if(Input.GetKey(KeyCode.D)) {
             inputVector.x += 1;
         }
-        
+
         inputVector = inputVector.normalized;
 
         var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        
+
         var tr = transform;
         tr.position += moveDir * (moveSpeed * Time.deltaTime);
-        
+
         IsWalking = moveDir != Vector3.zero;
-        
+
         const float rotateSpeed = 10f;
         tr.forward = Vector3.Slerp(tr.forward, moveDir, rotateSpeed * Time.deltaTime);
     }
--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 
-public class PlayerAnimator : MonoBehaviour
-{
+public class PlayerAnimator : MonoBehaviour {
     private static readonly int IsWalking = Animator.StringToHash("IsWalking");
     private Animator animator;
     [SerializeField] private Player player;
-    
+
     private void Awake() {
         animator = GetComponent<Animator>();
         animator.SetBool(IsWalking, player.IsWalking);
