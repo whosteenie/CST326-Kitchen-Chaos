b13392dbd0d3dce27520cfe50f61a30a2980a94b
Justin <justinmnmj@gmail.com>
2026-03-18 21:03:47 -0700

    new input system

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GameInput : MonoBehaviour {
+    
+    private PlayerInputActions playerInputActions;
+
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalized() {
+        var inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+        return inputVector;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,29 +2,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
     public bool IsWalking { get; private set; }
 
     private void Update() {
-        var inputVector = new Vector2(0, 0);
-
-        if(Input.GetKey(KeyCode.W)) {
-            inputVector.y += 1;
-        }
-
-        if(Input.GetKey(KeyCode.S)) {
-            inputVector.y -= 1;
-        }
-
-        if(Input.GetKey(KeyCode.A)) {
-            inputVector.x -= 1;
-        }
-
-        if(Input.GetKey(KeyCode.D)) {
-            inputVector.x += 1;
-        }
-
-        inputVector = inputVector.normalized;
+        var inputVector = gameInput.GetMovementVectorNormalized();
 
         var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 
