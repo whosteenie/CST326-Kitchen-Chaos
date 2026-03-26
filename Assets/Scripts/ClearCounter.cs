23f9dc3d66b95c31bc631aee921d8cd1dcc2ba0c
Justin <justinmnmj@gmail.com>
2026-03-26 00:24:19 -0700

    added counter variants

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,6 +4,18 @@ public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
     
     public override void Interact(Player player) {
-        
+        if(!HasKitchenObject()) {
+            if(player.HasKitchenObject()) {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else {
+                
+            }
+        } else {
+            if(player.HasKitchenObject()) {
+                
+            } else {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -8,8 +8,10 @@ public class ContainerCounter : BaseCounter
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
     
     public override void Interact(Player player) {
-        var kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        if(!player.HasKitchenObject()) {
+            var kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+        }
     }
 }
