72ed5d08b499e718e5d4bacc7dd1285290075f07
Justin <justinmnmj@gmail.com>
2026-03-26 00:08:04 -0700

    simplify counter logic

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,37 +1,9 @@
 using UnityEngine;
 
-public class ClearCounter : BaseCounter, IKitchenObjectParent {
-    [SerializeField] private Transform counterTopPoint;
+public class ClearCounter : BaseCounter {
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
-
-    private KitchenObject kitchenObject;
     
     public override void Interact(Player player) {
-        if(kitchenObject == null) {
-            var kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        } else {
-            kitchenObject.SetKitchenObjectParent(player);
-        }
-    }
-    
-    public Transform GetKitchenObjectFollowTransform() {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject incomingKitchenObject) {
-        kitchenObject = incomingKitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject() {
-        return kitchenObject;
-    }
-    
-    public void ClearKitchenObject() {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject() {
-        return kitchenObject != null;
+        
     }
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -1,38 +1,11 @@
 using UnityEngine;
 
-public class ContainerCounter : BaseCounter, IKitchenObjectParent
+public class ContainerCounter : BaseCounter
 {
-    [SerializeField] private Transform counterTopPoint;
     [SerializeField] private KitchenObjectSO kitchenObjectSo;
-
-    private KitchenObject kitchenObject;
     
     public override void Interact(Player player) {
-        if(kitchenObject == null) {
-            var kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab, counterTopPoint);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
-        } else {
-            kitchenObject.SetKitchenObjectParent(player);
-        }
-    }
-
-    public Transform GetKitchenObjectFollowTransform() {
-        return counterTopPoint;
-    }
-
-    public void SetKitchenObject(KitchenObject incomingKitchenObject) {
-        kitchenObject = incomingKitchenObject;
-    }
-
-    public KitchenObject GetKitchenObject() {
-        return kitchenObject;
-    }
-    
-    public void ClearKitchenObject() {
-        kitchenObject = null;
-    }
-
-    public bool HasKitchenObject() {
-        return kitchenObject != null;
+        var kitchenObjectTransform = Instantiate(kitchenObjectSo.prefab);
+        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
     }
 }
