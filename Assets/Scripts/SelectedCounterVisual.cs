a673a3effb6ca9ff6279cda2c885e1cd8bc160dd
Justin <justinmnmj@gmail.com>
2026-03-26 00:04:03 -0700

    fix selected visuals

--- Assets/Scripts/SelectedCounterVisual.cs
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -1,16 +1,15 @@
-using System;
 using UnityEngine;
 
 public class SelectedCounterVisual : MonoBehaviour {
     
-    [SerializeField] private ClearCounter clearCounter;
-    [SerializeField] private GameObject visualGameObject;
+    [SerializeField] private BaseCounter baseCounter;
+    [SerializeField] private GameObject[] visualGameObjectArray;
     private void Start() {
         Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
     }
 
     private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e) {
-        if(e.SelectedCounter == clearCounter) {
+        if(e.SelectedCounter == baseCounter) {
             Show();
         } else {
             Hide();
@@ -18,10 +17,14 @@ public class SelectedCounterVisual : MonoBehaviour {
     }
 
     private void Show() {
-        visualGameObject.SetActive(true);
+        foreach(var visualGameObject in visualGameObjectArray) {
+            visualGameObject.SetActive(true);
+        }
     }
 
     private void Hide() {
-        visualGameObject.SetActive(false);
+        foreach(var visualGameObject in visualGameObjectArray) {
+            visualGameObject.SetActive(false);
+        }
     }
 }
