768b87a189ccdabee3ce06491cb11da55a120e04
Justin <justinmnmj@gmail.com>
2026-03-27 01:53:56 -0700

    trash can

---
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TrashCounter : BaseCounter
+{
+    public override void Interact(Player player) {
+        if(player.HasKitchenObject()) {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
