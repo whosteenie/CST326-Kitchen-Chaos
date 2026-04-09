64cd4da3cfd209779f9e0238cd0052214f6b4a3f
Justin <justinmnmj@gmail.com>
2026-04-08 23:06:38 -0700

    main menu

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool isFirstUpdate = true;
+
+    private void Update() {
+        if(isFirstUpdate) {
+            isFirstUpdate = false;
+            
+            Loader.LoaderCallback();
+        }
+    }
+}
