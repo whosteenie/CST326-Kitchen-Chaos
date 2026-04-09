b7c16c8190d6cec070271eaa2c17799c6342dab1
Justin <justinmnmj@gmail.com>
2026-04-08 22:13:58 -0700

    countdown and game state

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -35,12 +35,14 @@ public class Player : MonoBehaviour, IKitchenObjectParent {
     }
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e) {
+        if(!GameManager.Instance.IsGamePlaying()) return;
         if(selectedCounter != null) {
             selectedCounter.InteractAlternate(this);
         }
     }
 
     private void GameInput_OnInteractAction(object sender, EventArgs e) {
+        if(!GameManager.Instance.IsGamePlaying()) return;
         if(selectedCounter != null) {
             selectedCounter.Interact(this);
         }
